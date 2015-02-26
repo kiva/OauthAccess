4c3975993a9bd565d9bc402589ca9b71f1117aff
uglymunky <gabriel@uglymunky.com>
2015-02-26 08:16:30 -0800

    update travis + coveralls

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,14 @@ module.exports = function(grunt) {
 		}
 
 
+		, coveralls: {
+			basic: {
+				src: 'test/coverage/lcov.info'
+				, force: true
+			}
+		}
+
+
 		, jshint: {
 			options: {
 				jshintrc: '.jshintrc'
@@ -73,6 +81,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-rigger');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-coveralls');
 
 	grunt.registerTask('test', ['jshint', 'buster']);
 	grunt.registerTask('build', ['rig', 'uglify']);
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
+[![Build Status](https://travis-ci.org/kiva/oauthaccess.png)](https://travis-ci.org/kiva/oauthaccess)
+[![Coverage Status](https://coveralls.io/repos/kiva/OauthAccess/badge.svg)](https://coveralls.io/r/kiva/OauthAccess)
 # OauthAccess
-
 
 ```
 // Create an OauthAccess instance
